33cb463ede3a43dd0ecffb5de9f3f03f760bd775
Lara Coding <78417151+JoyofCodingLife@users.noreply.github.com>
2021-03-21 13:03:12 +1100

    Update 5 - stuck on JS for  results to store and show

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// Define variables
+// List of all Variables
     // NavBar
      var timeLeft = document.getElementById("seconds-left");
      var clock = document.getElementById("timer");
@@ -25,21 +25,22 @@
      var initialsInput = document.getElementById("initials-input");
      var submitBtn = document.getElementById("submit-btn");
 
-    //Highscores Screen
+    // Highscores Screen
      var highscoresSection = document.getElementById("highscores-section");
+     var viewHighscore = document.getElementById("view-highscores")
      var listOfHighscores = document.getElementById("list-of-highscores");
      var tryAgainBtn = document.getElementById("try-again-btn");
      var clearAllBtn = document.getElementById("clear-all-btn");
 
-// Other Used Variable
-var timeInterval;
-var timeGiven = 120;
-var correctAnswer = 0;
-var questionNumber = 0;
-var questionIndex = 0;
-var scoreResult;
+    // Other Used Variable
+     var timeInterval;
+     var timeGiven = 120;
+     var correctAnswer = 0;
+     var questionNumber = 0;
+     var questionIndex = 0;
+     var scoreResult;
 
-// Define Questions
+// List of Questions
 var allQuestions = [
     {
         question: "Who invented JavaScript?",
@@ -96,95 +97,177 @@ var allQuestions = [
     }
 ]
 
-// Functions based on Acceptance Criterias (listed to help create correct functions and work flow)
+// List of all Functions
 
-//When Start Button is clicked
-startBtn.addEventListener("click", startNewQuiz);
+//START SECTION
 
-//Then a timer starts 
-function startNewQuiz () {
-    timeGiven = 120;
-    timeLeft.textContent = timeGiven;
-    questionIndex = 0;
-    initialsInput = "";
+ // When Start Button is clicked
+ startBtn.addEventListener("click", startNewQuiz);
 
-    startSection.style.display = "none";
-    questionsSection.style.display = "flex";
+// QUESTIONS SECTIONS
+  
+  // Then a timer starts 
+  function startNewQuiz () {
+      timeGiven = 120;
+      timeLeft.textContent = timeGiven + " seconds left";
+      questionIndex = 0;
+      initialsInput = "";
+      // Hide Start Section, show Questions Section
+      startSection.style.display = "none";
+      questionsSection.style.display = "flex";
+  
+      var startTimer = setInterval (function () {
+          timeGiven--;
+          timeLeft.textContent = timeGiven + " seconds left";
+          if (timeGiven === 1) {
+              timeLeft.textContent = timeGiven + " second left"
+          } 
+          // if time run out, then run Game Over functions
+          if (timeGiven <= 0) {
+              clearInterval(startTimer);
+              if (questionIndex < allQuestions.length -1) {
+                  gameOver();
+              }
+          }
+      }, 1000);
+  
+      showupQuiz();
+  }
+  
+  // And quiz starts, asking question by question
+  function showupQuiz () {
+      nextQuestion ();
+  }
+  
+  function nextQuestion () {
+      questionTitle.textContent = allQuestions[questionIndex].question;
+      choice1.textContent = allQuestions[questionIndex].choices[0];
+      choice2.textContent = allQuestions[questionIndex].choices[1];
+      choice3.textContent = allQuestions[questionIndex].choices[2];
+      choice4.textContent = allQuestions[questionIndex].choices[3];
+  }
+  
+  // Event Listeners for Choice Buttons
+  choice1.addEventListener("click", choose0);
+  choice2.addEventListener("click", choose1);
+  choice3.addEventListener("click", choose2);
+  choice4.addEventListener("click", choose3);
+  
+  // Functions for Choice Buttons
+  function choose0 () {answerCheck(0);}
+  function choose1 () {answerCheck(1);}
+  function choose2 () {answerCheck(2);}
+  function choose3 () {answerCheck(3);}
 
-    var startTimer = setInterval (function () {
-        timeGiven--;
-        timeLeft.textContent = timeGiven;
-        if (timeGiven <= 0) {
-            clearInterval(startTimer);
-            if (questionIndex < allQuestions.length -1) {
-                gameOver();
-            }
-        }
-    }, 1000);
 
-    showupQuiz();
+
+   // When question is answered, it shows if it was correct or wrong
+  function answerCheck (answer) {
+      showAnswer.style.display = "block";
+      // if correct answer, then add 1 score to final score
+      if (allQuestions[questionIndex].answer === allQuestions[questionIndex].choices[answer]){
+          correctAnswer++;
+          showAnswer.textContent = "YES! Correct!"
+      } 
+      // if wrong then deduct 10 second from time left
+      else {
+          timeGiven -=10;
+          timeLeft.textContent = timeGiven + " seconds left";
+          showAnswer.textContent = "OOPS...Wrong! The correct answer is: " + allQuestions[questionIndex].answer;
+      }
+      // then next question is asked
+      questionIndex++;
+      if (questionIndex < allQuestions.length){
+          nextQuestion();
+      }
+      // if no more questions is left, then run Game Over function
+      else {
+          gameOver();
+      }
+  }
+
+// RESULTS SECTIONS
+
+  // Game Over function 
+  function gameOver () {
+      // Hide Question Section, show Results Section
+      questionsSection.style.display = "none";
+      resultsSection.style.display = "flex";
+      // show final score of correct answers
+      finalScore.textContent = correctAnswer; 
+      // stop the timer
+      clock.textContent = "~ Game Over ~"
+  }
+  
+  // Event listener for Submit Button
+  submitBtn.addEventListener("click", function(event){ 
+      storeResults(event);
+  });
+
+
+  // Enter initials and store highscore
+  function storeResults (event) {
+      event.preventBlank();
+  
+      // alert if blank
+      if (initialsInput.value === "") {
+          alert ("Stranger, please enter your initials.");
+          return;
+      }
+  
+      // store initials and highscores
+      var highscoreStorage = localStorage.getItem("highscores");
+      var scoresArray;
+  
+      if (highscoreStorage === null) {
+          scoresArray = [];
+      } else {
+          scoresArray = JSON.parse(highscoreStorage)
+      }
+  
+      var playerScore = {
+          initials: initialInput.value,
+          score: finalScore.textContent
+      };
+  
+      console.log(playerScore);
+      scoresArray.push(playerScore);
+  
+      // stringify array in order to store in local
+      var scoresArrayString = JSON.stringify(scoresArray);
+      window.localStorage.setItem("highscores", scoresArrayString);
+      
+    // list current highscores
+    listHighscores();
 }
 
-//And Question Section shows up with multiple choice questions 
-function showupQuiz () {
-    nextQuestion ();
-}
+// HIGHSCORES SECTION
 
-function nextQuestion () {
-    questionTitle.textContent = allQuestions[questionIndex].question;
-    choice1.textContent = allQuestions[questionIndex].choices[0];
-    choice2.textContent = allQuestions[questionIndex].choices[1];
-    choice3.textContent = allQuestions[questionIndex].choices[2];
-    choice4.textContent = allQuestions[questionIndex].choices[3];
-}
+  //Event listener for View Highscores
+  viewHighscore.addEventListener("click", function(event) { 
+    listHighscores(event);
+    });
 
-// Event Listeners for Choice Buttons
-choice1.addEventListener("click", choose0);
-choice2.addEventListener("click", choose1);
-choice3.addEventListener("click", choose2);
-choice4.addEventListener("click", choose3);
+  var i = 0;
+  function listHighscores() {
+    // Hide Result Section, show Results Section
+    resultsSection.style.display = "hide"
+    highscoresSection.style.display = "flex";
 
-// Functions for Choice Buttons
-function choose0 () {answerCheck(0);}
-function choose1 () {answerCheck(1);}
-function choose2 () {answerCheck(2);}
-function choose3 () {answerCheck(3);}
+    var highscoreStorage = localStorage.getItem("highscores");
 
-
-
-//When question is answered, it shows if it was correct or wrong
-function answerCheck (answer) {
-    showAnswer.style.display = "block";
-    // if correct answer, then add 1 score to final score
-    if (allQuestions[questionIndex].answer === allQuestions[questionIndex].choices[answer]){
-        correctAnswer++;
-        showAnswer.textContent = "YES! Correct!"
-    } 
-    // if wrong then deduct 10 second from time left
-    else {
-        timeGiven -=10;
-        timeLeft.textContent = timeGiven;
-        showAnswer.textContent = "OOPS...Wrong! The correct answer is: " + allQuestions[questionIndex].answer;
+    // check if there is any saved in local storage
+    if (highscoreStorage === null) {
+        return;
     }
-    //then next question is asked
-    questionIndex++;
-    if (questionIndex < allQuestions.length){
-        nextQuestion();
-    }
-    //if no more questions is left, then run Game Over function
-    else {
-        gameOver();
-    }
-}
-// Game Over function 
-function gameOver () {
-    questionsSection.style.display = "none";
-    resultsSection.style.display = "flex";
-    //show final score of correct answers
-    finalScore.textContent = correctAnswer; 
-    //stop the timer
-    clock.textContent = "~ Game Over ~"
-}
+    console.log(highscoreStorage);
 
-// Enter initials and store highscore
+    var savedHighscores = JSON.parse(highscoreStorage);
+
+    for (; i < savedHighscores.length; i++) {
+        var newHighscore = document.createElement("p");
+        newHighscore.innerHTML = "Player " + savedHighscores[i].initials + " = " + savedHighscores[i].score;
+        listOfHighscores.appendChild(newHighscore);
+    }
+  }
 
